2a6a09c89ff84cb42ae813720ba1fcd19940f64a
ibtechraj <krs30018@gmail.com>
2024-12-19 11:51:46 +0530

    about next batch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -74,7 +74,7 @@ function Home(signedIn, setSignedIn) {
 
         <div className="row col-8 offset-2 py-5 d-flex text-center">
           <div className="b" style={{ margin: 'auto', color: '#4221B9' }}>
-            We successfully launched our first batch from this platform on 3rd June 2024. We have quickly started a new batch on 30th July 2024. If you register with us before hand we can quickly inform you when the new batch starts.
+            We are starting our next batch in January 2025. Sign up today and we will inform you the exact date.
 
           </div>
         </div>
