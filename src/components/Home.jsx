e44dbf01c04db7fa08deb00c02bdaea1372a23ca
ibtechraj <krs30018@gmail.com>
2026-06-27 09:15:46 +0530

    start date

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -74,7 +74,7 @@ function Home(signedIn, setSignedIn) {
 
         <div className="row col-8 offset-2 py-5 d-flex text-center">
           <div className="b" style={{ margin: 'auto', color: '#4221B9' }}>
-            New batch starting 29th June 2026. To learn C Programming and get confident in writing programs independently, sign up today.
+            New batch is starting on 29th June 2026 7:00 PM - 8:30 PM. To learn C Programming and get confident in writing programs independently, sign up today.
 
           </div>
           <div className="b" style={{ margin: 'auto', color: '#de24d8' }}>
